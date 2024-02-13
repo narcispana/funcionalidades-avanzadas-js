0ae4cf4e0323bfd1b2491e0bb35ab0cf1c4ce3ec
narcispana <narcisscoot10@gmail.com>
2024-02-13 13:03:19 +0100

    Añadir ejercicio-01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,6 @@
-// Escribe tu código aquí:
+
+ const numbers = [2, 3, 5, 7, 10]
+
+ const numeroAlCuadrado = numbers.map((number)=> number * number)
+
+ console.log('numeros al cuadrado', numeroAlCuadrado)
